2427d7da92e42d96f0ee230b2c429da02b4a8569
andrewnguyen22 <andrew@ginchu.com>
2025-07-12 19:49:23 -0400

    removed lock

--- controller/consensus.go
+++ controller/consensus.go
@@ -303,11 +303,6 @@ func (c *Controller) ListenForConsensus() {
 		// execute in a sub-function to unify error handling and enable 'defer' functionality
 		if err := func() (err lib.ErrorI) {
 			c.log.Debugf("Handling consensus message")
-			//defer lib.TimeTrack(c.log, time.Now())
-			// lock the controller for thread safety
-			c.Lock()
-			// once the handler completes, unlock
-			defer c.Unlock()
 			// create a new 'consensus message' to unmarshal the bytes to
 			bftMsg := new(bft.Message)
 			// try to unmarshal into a consensus message
